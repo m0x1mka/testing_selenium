bda327b1fc1fd1c5bb0ecba42317faf856a2c9a9
Classics <maks.zablocky@gmail.com>
2023-06-23 09:53:48 +0300

    Changed link in test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
