957246bdcda3137ea4f17b279f85afa30be7fe1f
Classics <maks.zablocky@gmail.com>
2023-06-23 10:07:23 +0300

    Changed link in test_main_page.py and css_selector

--- test_main_page.py
+++ test_main_page.py
@@ -6,5 +6,6 @@ from selenium.webdriver.common.by import By
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "button[class='btn btn-lg btn-primary btn-add-to-basket'']")
-    login_link.click()
+    button_to_add_to_basket = browser.find_element(By.CSS_SELECTOR,
+                                                   "button[class='btn btn-lg btn-primary btn-add-to-basket'']")
+    assert button_to_add_to_basket is not None, "Your selectors are wrong"
