085901a9f0859e99c04e03ed872ee11004be5084
Classics <maks.zablocky@gmail.com>
2023-06-23 10:14:36 +0300

    css selectors changed

--- test_main_page.py
+++ test_main_page.py
@@ -7,5 +7,5 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     button_to_add_to_basket = browser.find_element(By.CSS_SELECTOR,
-                                                   "button[class='btn btn-lg btn-primary btn-add-to-basket'']")
+                                                   "button[article form button[type='submit']]")
     assert button_to_add_to_basket is not None, "Your selectors are wrong"
