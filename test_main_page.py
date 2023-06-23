a7116b05d1a5ededc7646688d9ba0ce9597b79b6
Classics <maks.zablocky@gmail.com>
2023-06-23 10:04:51 +0300

    Changed link in test_main_page.py and css_selector

--- test_main_page.py
+++ test_main_page.py
@@ -6,5 +6,5 @@ from selenium.webdriver.common.by import By
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link = browser.find_element(By.CSS_SELECTOR, "button[class='btn btn-lg btn-primary btn-add-to-basket'']")
     login_link.click()
