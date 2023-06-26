43ef9e14e68295cf7c2e9a6117724ed9f3153ef7
Classics <maks.zablocky@gmail.com>
2023-06-26 19:25:35 +0300

    program changed at all

---
+++ pages/locators.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.ID, "registration_link")
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,7 @@
 from .base_page import BasePage
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
 
 
 class MainPage(BasePage):
