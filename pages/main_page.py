199922002236c3d9653d477bc97e3da97185298f
Classics <maks.zablocky@gmail.com>
2023-06-26 18:54:12 +0300

    created class MainPage

---
+++ pages/main_page.py
@@ -0,0 +1,8 @@
+from .base_page import BasePage
+
+
+class MainPage(BasePage):
+    class MainPage(BasePage):
+        def go_to_login_page(self):
+            login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+            login_link.click()
