1c1a6aa7dbd5458465e1e24b81190ed5ef74353d
Classics <maks.zablocky@gmail.com>
2023-06-26 10:53:38 +0300

    --browser deleted

--- conftest.py
+++ conftest.py
@@ -3,8 +3,6 @@ from selenium import webdriver
 
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
-                     help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default=None,
                      help="Choose language")
 
