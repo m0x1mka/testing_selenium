adf9b6a256e04c703092a481fe9712dd42a0f235
Classics <maks.zablocky@gmail.com>
2023-06-26 11:01:18 +0300

    wrong method renamed

--- test_items.py
+++ test_items.py
@@ -8,7 +8,7 @@ def test_guest_can_go_to_login_page(language):
     browser = webdriver.Chrome()
     link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sllep(30)
+    time.sleep(30)
     button_to_add_to_basket = browser.find_element(By.CSS_SELECTOR,
                                                    "article form button[type='submit']")
     assert button_to_add_to_basket is not None, "Here is no button!!!"
