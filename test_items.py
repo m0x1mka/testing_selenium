e5e24c09711848ad25076b6eef75d02048885bb9
Classics <maks.zablocky@gmail.com>
2023-06-26 19:27:09 +0300

    program changed at all

--- test_items.py
+++ test_items.py
@@ -6,14 +6,14 @@ import time
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = " http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
+    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
 
 
 def test_guest_should_see_login_link(browser):
-    link = " http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
+    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
