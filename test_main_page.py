4bc2868ee04dac85d9fe6f236d6bfd5dc0b0fcf0
Classics <maks.zablocky@gmail.com>
2023-06-26 10:46:46 +0300

    link changed

--- test_main_page.py
+++ test_main_page.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 
 
 def test_guest_can_go_to_login_page(browser, language):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/" + language
+    link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
     browser.get(link)
     button_to_add_to_basket = browser.find_element(By.CSS_SELECTOR,
                                                    "article form button[type='submit']")
