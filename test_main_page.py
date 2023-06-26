20aea51e58ae87a2775174cabee88a3249302a2a
Classics <maks.zablocky@gmail.com>
2023-06-26 10:56:06 +0300

    module time imported

--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,14 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 
 def test_guest_can_go_to_login_page(language):
     browser = webdriver.Chrome()
     link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
     browser.get(link)
+    time.sllep(30)
     button_to_add_to_basket = browser.find_element(By.CSS_SELECTOR,
                                                    "article form button[type='submit']")
     assert button_to_add_to_basket is not None, "Here is no button!!!"
