c91ccb2780e1a3f03b5785c2fe9b58cec35ca306
Classics <maks.zablocky@gmail.com>
2023-06-26 10:51:36 +0300

    "browser" fixture was deleted

--- conftest.py
+++ conftest.py
@@ -10,24 +10,6 @@ def pytest_addoption(parser):
 
 
 @pytest.fixture(scope="function")
-def browser(request):
-    browser_name = request.config.getoption("browser_name")
-
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
-
-
-@pytest.fixture(scope="function")
 def language(request):
     page_language = request.config.getoption("language")
     yield page_language
--- test_main_page.py
+++ test_main_page.py
@@ -3,7 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-def test_guest_can_go_to_login_page(browser, language):
+def test_guest_can_go_to_login_page(language):
+    browser = webdriver.Chrome()
     link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
     browser.get(link)
     button_to_add_to_basket = browser.find_element(By.CSS_SELECTOR,
