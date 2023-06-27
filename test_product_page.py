a77fafb2e264850356c28ed2562a0ca19a115605
Classics <maks.zablocky@gmail.com>
2023-06-27 10:40:34 +0300

    4_3_3 stepik task completed

--- test_product_page.py
+++ test_product_page.py
@@ -7,7 +7,7 @@ import time
 
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     time.sleep(2)
