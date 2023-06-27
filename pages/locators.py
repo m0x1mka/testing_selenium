c2eb1bd35d273bb86be96295c8efb34755b5798e
Classics <maks.zablocky@gmail.com>
2023-06-27 10:39:05 +0300

    4_3_2 stepik task updated

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,11 @@ class MainPageLocators:
 
 class AddToBasketLocators:
     ADD_BOOK_TO_BASKET = (By.CSS_SELECTOR, 'form[id="add_to_basket_form"] button[type="submit"]')
+
+
+class BasketLocators:
+    BOOK_TITLE = (By.CSS_SELECTOR, 'div[id="content_inner"] h1')
+    BOOK_PRICE = (By.CSS_SELECTOR, 'div[id="content_inner"] p')
+    BASKET_BUTTON = (By.CSS_SELECTOR, 'span[class="btn-group"] a')
+    FULL_PRICE = (By.CSS_SELECTOR, 'p[class="price_color align-right"]')
+    BASKET_BOOK_TITLE = (By.CSS_SELECTOR, 'div[class="col-sm-4"] a')
--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from .pages.product_page import ProductPage
+from .pages.locators import BasketLocators
 import time
 
 
@@ -14,3 +15,15 @@ def test_guest_can_add_product_to_basket(browser):
     time.sleep(2)
     page.solve_quiz_and_get_code()
     time.sleep(2)
+    book_name = browser.find_element(*BasketLocators.BOOK_TITLE).text
+    book_price = browser.find_element(*BasketLocators.BOOK_PRICE).text
+
+    basket_button = browser.find_element(*BasketLocators.BASKET_BUTTON)
+    basket_button.click()
+    time.sleep(2)
+
+    full_price = browser.find_element(*BasketLocators.FULL_PRICE).text
+    book_basket_name = browser.find_element(*BasketLocators.BASKET_BOOK_TITLE).text
+    print(book_name, book_price, book_basket_name, full_price)
+
+    assert book_name == book_basket_name and book_price == full_price, "Something is wrong"
